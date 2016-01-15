eb5347317a784f8dbaae6a5fe6d22c75ac4f6378
Marcelo Gornstein <marcelog@gmail.com>
2016-01-15 10:21:41 -0300

    first test skeleton

--- test/ibrowse_client_SUITE.erl
+++ test/ibrowse_client_SUITE.erl
@@ -1,6 +1,24 @@
 -module(ibrowse_client_SUITE).
 
 -export([all/0]).
+-export([init_per_suite/1, end_per_suite/1]).
+-export([
+  can_request_with_right_headers_and_body/1
+]).
 
 all() ->
-  [].
+  [
+    can_request_with_right_headers_and_body
+  ].
+
+init_per_suite(Config) ->
+  Config.
+
+end_per_suite(_Config) ->
+  ok.
+
+can_request_with_right_headers_and_body(_Config) ->
+  spawn_link(fun() ->
+    ok
+  end),
+  ok.
